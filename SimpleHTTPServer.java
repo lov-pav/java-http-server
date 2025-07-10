48522d149c5d2a43b27abf19b799eecf5e55a133
mrlovyou <nightprofile.love@gmail.com>
2025-07-10 14:42:36 -0500

    server can accept connections but does not send response back to client.

---
+++ SimpleHTTPServer.java
@@ -0,0 +1,29 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class SimpleHTTPServer {
+
+
+    public static void main (String[] args) throws IOException{
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080....");
+        while (true) {
+
+            final Socket clientSocket  = server.accept();
+            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+
+        }
+    }
+}
